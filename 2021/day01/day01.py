ca68ab26391999e159e7a69db3e4482018af6bbc
Johan Wallinder <johan.wallinder@funnel.io>
2021-12-06 13:40:17 +0100

    updates to part1 solution

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -1,21 +1,37 @@
 from input import part1_input as input
+from time import perf_counter_ns
+
+NS_TO_MICRO = 1000
 
 
+def solver(fn):
+    def wrap(*args, **kwargs):
+        start = perf_counter_ns()
+        res = fn(*args, **kwargs)
+        elapsed_time = perf_counter_ns() - start
+        print(f"{fn.__name__}:")
+        print(f"Answer is: {res}")
+        print(f"Took: {elapsed_time / NS_TO_MICRO} µs\n")
+
+    return wrap
+
+
+@solver
 def part1():
-    print(input)
+    # print(input)
     lines = input.splitlines()
     prev = int(lines[0])
-    increased= 0
+    increased = 0
 
     for line in lines[1:]:
         line = int(line)
-        print(f"comparing {prev=}, {line=}")
+        # print(f"comparing {prev=}, {line=}")
         if line > prev:
             increased += 1
-            print(f"{increased=}")
+            # print(f"{increased=}")
         prev = line
     return increased
 
 
 if __name__ == '__main__':
-    print(part1())
+    part1()
