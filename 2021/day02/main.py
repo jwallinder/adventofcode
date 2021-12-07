0fe5bd22e1c28d91f49f9aa3c7a2118672c59cde
Johan Wallinder <johan.wallinder@funnel.io>
2021-12-07 09:02:32 +0100

    day2 - part2

--- 2021/day02/main.py
+++ 2021/day02/main.py
@@ -36,9 +36,28 @@ def process_line(line):
 
 
 @solver
-def part2():
-    pass
+def part2(test_input=False):
 
+    file = "test_input.txt" if test_input else "part1.txt"
+
+    with open(file) as f:
+        lines = f.read().splitlines()
+        # print(lines)
+
+    forward = 0
+    depth = 0
+    aim = 0
+
+    for line in lines:
+        aim_delta, forward_delta = process_line(line)
+        # print("-------------")
+        # print(f"{aim_delta=}, {forward_delta=}")
+
+        forward += forward_delta
+        aim += aim_delta
+        depth += aim*forward_delta
+        # print(f"{forward=}, {aim=}, {depth=}")
+    return forward * depth
 
 if __name__ == '__main__':
     part1()
