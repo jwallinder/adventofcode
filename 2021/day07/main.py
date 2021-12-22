20048da2243c207ddd6e00bff3872a71822f6d64
Johan Wallinder <johan.wallinder@funnel.io>
2021-12-22 13:54:55 +0100

    day07 - part2

--- 2021/day07/main.py
+++ 2021/day07/main.py
@@ -16,10 +16,10 @@ def load_from_input(test_input=False):
 @solver
 def part1(test_input=False):
     input = list(load_from_input(test_input))
-    print(f"{input=}")
+    # print(f"{input=}")
     max_val = max(input)
     min_val = min(input)
-    print(f"{min_val} :: {max_val}")
+    # print(f"{min_val} :: {max_val}")
     x_pos = min_val
     sum_steps = 99999999
 
@@ -31,18 +31,36 @@ def part1(test_input=False):
             sum_steps = current_sum_steps
             x_pos = pos
 
-    print(f"finding minimun at pos: {x_pos}, sum_steps: {sum_steps}")
+    # print(f"finding minimun at pos: {x_pos}, sum_steps: {sum_steps}")
     return sum_steps
 
 
 @solver
 def part2(test_input=False):
-    pass
+    input = list(load_from_input(test_input))
+    # print(f"{input=}")
+    max_val = max(input)
+    min_val = min(input)
+    # print(f"{min_val} :: {max_val}")
+    x_pos = min_val
+    sum_steps = 99999999999
+
+    for pos in range(min_val, max_val+1):
+        steps = [abs(pos-x) for x in input]
+        steps = [x*(x+1)/2 for x in steps]
+        #print(f"move to pos: {pos} - {steps}")
+        current_sum_steps = sum(steps)
+        if current_sum_steps < sum_steps:
+            sum_steps = current_sum_steps
+            x_pos = pos
+
+    #print(f"finding minimun at pos: {x_pos}, sum_steps: {sum_steps}")
+    return int(sum_steps)
 
 
 
 if __name__ == '__main__':
     assert part1(True) == 37
-    assert part1(False) == "part1"
-    assert part2(True) == "part2 - test"
-    assert part2(False) == "part2"
+    assert part1(False) == 351901
+    assert part2(True) == 168
+    assert part2(False) == 101079875
